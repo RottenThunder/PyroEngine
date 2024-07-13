ef24ce2031858f029450b0870ca0d422b92fc12e
Ilya Polyaev <polyaev.ilya.23@gmail.com>
2024-07-13 13:15:58 +0100

    Added File Writer

---
+++ PyroEngine/src/PyroEngine/File/FileWriter.cpp
@@ -0,0 +1,308 @@
+#include "PyroEnginePCH.h"
+#include "FileWriter.h"
+
+namespace PyroEngine
+{
+	FileWriter::FileWriter()
+		: m_FilePath(std::filesystem::current_path()), m_FileStream(), m_IsAlwaysAppending(false)
+	{
+	}
+
+	FileWriter::~FileWriter()
+	{
+		m_FileStream.close();
+	}
+
+	std::filesystem::path FileWriter::GetFilePath() const
+	{
+		return m_FilePath;
+	}
+
+	bool FileWriter::GetIsAlwaysAppending() const
+	{
+		return m_IsAlwaysAppending;
+	}
+
+	bool FileWriter::Open(const std::filesystem::path& path, bool alwaysAppend)
+	{
+		m_FileStream.close();
+		m_IsAlwaysAppending = alwaysAppend;
+		if (m_IsAlwaysAppending)
+		{
+			m_FileStream.open(path, std::ios::binary | std::ios::app);
+			if (m_FileStream.is_open())
+			{
+				m_FilePath = std::filesystem::absolute(path);
+				return true;
+			}
+			else
+			{
+				m_FilePath = std::filesystem::current_path();
+				m_IsAlwaysAppending = false;
+				return false;
+			}
+		}
+		else
+		{
+			size_t sizeOfFile = 0;
+
+			if (std::filesystem::is_directory(path))
+			{
+				m_FilePath = std::filesystem::current_path();
+				return false;
+			}
+
+			if (std::filesystem::exists(path))
+				sizeOfFile = std::filesystem::file_size(path);
+
+			if (sizeOfFile == 0)
+			{
+				m_FileStream.open(path, std::ios::binary);
+				if (m_FileStream.is_open())
+				{
+					m_FilePath = std::filesystem::absolute(path);
+					return true;
+				}
+				else
+				{
+					m_FilePath = std::filesystem::current_path();
+					return false;
+				}
+			}
+			else if (sizeOfFile > PYRO_FILE_WRITER_FILE_LIMIT)
+			{
+				m_FilePath = std::filesystem::current_path();
+				return false;
+			}
+			else
+			{
+				std::ifstream inputStream;
+				inputStream.open(path, std::ios::binary);
+				if (inputStream.is_open())
+				{
+					uint8_t* fileContents = new uint8_t[sizeOfFile];
+					inputStream.read(reinterpret_cast<char*>(fileContents), sizeOfFile);
+					inputStream.close();
+					m_FileStream.open(path, std::ios::binary);
+					if (m_FileStream.is_open())
+					{
+						m_FileStream.write(reinterpret_cast<char*>(fileContents), sizeOfFile);
+						m_FileStream.seekp(0, std::ios::beg);
+						::operator delete(fileContents, sizeOfFile);
+						m_FilePath = std::filesystem::absolute(path);
+						return true;
+					}
+					else
+					{
+						::operator delete(fileContents, sizeOfFile);
+						m_FilePath = std::filesystem::current_path();
+						return false;
+					}
+				}
+				else
+				{
+					m_FilePath = std::filesystem::current_path();
+					return false;
+				}
+			}
+		}
+	}
+
+	void FileWriter::Close()
+	{
+		m_FileStream.close();
+		m_FilePath = std::filesystem::current_path();
+		m_IsAlwaysAppending = false;
+	}
+
+	bool FileWriter::IsOpen() const
+	{
+		return m_FileStream.is_open();
+	}
+
+	void FileWriter::SeekPosition(size_t pos)
+	{
+		m_FileStream.seekp(pos, std::ios::beg);
+	}
+
+	size_t FileWriter::GetPosition()
+	{
+		return m_FileStream.tellp();
+	}
+
+	void FileWriter::GoToStart()
+	{
+		m_FileStream.seekp(0, std::ios::beg);
+	}
+
+	void FileWriter::GoToEnd()
+	{
+		m_FileStream.seekp(0, std::ios::end);
+	}
+
+	void FileWriter::WriteInt8LE(int8_t integer)
+	{
+		uint8_t bytes[1];
+		bytes[0] = integer;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 1);
+	}
+
+	void FileWriter::WriteInt16LE(int16_t integer)
+	{
+		uint8_t bytes[2];
+		bytes[0] = integer & 0x00FF;
+		bytes[1] = (integer & 0xFF00) >> 8;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 2);
+	}
+
+	void FileWriter::WriteInt32LE(int32_t integer)
+	{
+		uint8_t bytes[4];
+		bytes[0] = integer & 0x000000FF;
+		bytes[1] = (integer & 0x0000FF00) >> 8;
+		bytes[2] = (integer & 0x00FF0000) >> 16;
+		bytes[3] = (integer & 0xFF000000) >> 24;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 4);
+	}
+
+	void FileWriter::WriteInt64LE(int64_t integer)
+	{
+		uint8_t bytes[8];
+		bytes[0] = integer & 0x00000000000000FF;
+		bytes[1] = (integer & 0x000000000000FF00) >> 8;
+		bytes[2] = (integer & 0x0000000000FF0000) >> 16;
+		bytes[3] = (integer & 0x00000000FF000000) >> 24;
+		bytes[4] = (integer & 0x000000FF00000000) >> 32;
+		bytes[5] = (integer & 0x0000FF0000000000) >> 40;
+		bytes[6] = (integer & 0x00FF000000000000) >> 48;
+		bytes[7] = (integer & 0xFF00000000000000) >> 56;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 8);
+	}
+
+	void FileWriter::WriteUInt8LE(uint8_t integer)
+	{
+		uint8_t bytes[1];
+		bytes[0] = integer;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 1);
+	}
+
+	void FileWriter::WriteUInt16LE(uint16_t integer)
+	{
+		uint8_t bytes[2];
+		bytes[0] = integer & 0x00FF;
+		bytes[1] = (integer & 0xFF00) >> 8;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 2);
+	}
+
+	void FileWriter::WriteUInt32LE(uint32_t integer)
+	{
+		uint8_t bytes[4];
+		bytes[0] = integer & 0x000000FF;
+		bytes[1] = (integer & 0x0000FF00) >> 8;
+		bytes[2] = (integer & 0x00FF0000) >> 16;
+		bytes[3] = (integer & 0xFF000000) >> 24;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 4);
+	}
+
+	void FileWriter::WriteUInt64LE(uint64_t integer)
+	{
+		uint8_t bytes[8];
+		bytes[0] = integer & 0x00000000000000FF;
+		bytes[1] = (integer & 0x000000000000FF00) >> 8;
+		bytes[2] = (integer & 0x0000000000FF0000) >> 16;
+		bytes[3] = (integer & 0x00000000FF000000) >> 24;
+		bytes[4] = (integer & 0x000000FF00000000) >> 32;
+		bytes[5] = (integer & 0x0000FF0000000000) >> 40;
+		bytes[6] = (integer & 0x00FF000000000000) >> 48;
+		bytes[7] = (integer & 0xFF00000000000000) >> 56;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 8);
+	}
+
+	void FileWriter::WriteInt8BE(int8_t integer)
+	{
+		uint8_t bytes[1];
+		bytes[0] = integer;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 1);
+	}
+
+	void FileWriter::WriteInt16BE(int16_t integer)
+	{
+		uint8_t bytes[2];
+		bytes[1] = integer & 0x00FF;
+		bytes[0] = (integer & 0xFF00) >> 8;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 2);
+	}
+
+	void FileWriter::WriteInt32BE(int32_t integer)
+	{
+		uint8_t bytes[4];
+		bytes[3] = integer & 0x000000FF;
+		bytes[2] = (integer & 0x0000FF00) >> 8;
+		bytes[1] = (integer & 0x00FF0000) >> 16;
+		bytes[0] = (integer & 0xFF000000) >> 24;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 4);
+	}
+
+	void FileWriter::WriteInt64BE(int64_t integer)
+	{
+		uint8_t bytes[8];
+		bytes[7] = integer & 0x00000000000000FF;
+		bytes[6] = (integer & 0x000000000000FF00) >> 8;
+		bytes[5] = (integer & 0x0000000000FF0000) >> 16;
+		bytes[4] = (integer & 0x00000000FF000000) >> 24;
+		bytes[3] = (integer & 0x000000FF00000000) >> 32;
+		bytes[2] = (integer & 0x0000FF0000000000) >> 40;
+		bytes[1] = (integer & 0x00FF000000000000) >> 48;
+		bytes[0] = (integer & 0xFF00000000000000) >> 56;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 8);
+	}
+
+	void FileWriter::WriteUInt8BE(uint8_t integer)
+	{
+		uint8_t bytes[1];
+		bytes[0] = integer;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 1);
+	}
+
+	void FileWriter::WriteUInt16BE(uint16_t integer)
+	{
+		uint8_t bytes[2];
+		bytes[1] = integer & 0x00FF;
+		bytes[0] = (integer & 0xFF00) >> 8;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 2);
+	}
+
+	void FileWriter::WriteUInt32BE(uint32_t integer)
+	{
+		uint8_t bytes[4];
+		bytes[3] = integer & 0x000000FF;
+		bytes[2] = (integer & 0x0000FF00) >> 8;
+		bytes[1] = (integer & 0x00FF0000) >> 16;
+		bytes[0] = (integer & 0xFF000000) >> 24;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 4);
+	}
+
+	void FileWriter::WriteUInt64BE(uint64_t integer)
+	{
+		uint8_t bytes[8];
+		bytes[7] = integer & 0x00000000000000FF;
+		bytes[6] = (integer & 0x000000000000FF00) >> 8;
+		bytes[5] = (integer & 0x0000000000FF0000) >> 16;
+		bytes[4] = (integer & 0x00000000FF000000) >> 24;
+		bytes[3] = (integer & 0x000000FF00000000) >> 32;
+		bytes[2] = (integer & 0x0000FF0000000000) >> 40;
+		bytes[1] = (integer & 0x00FF000000000000) >> 48;
+		bytes[0] = (integer & 0xFF00000000000000) >> 56;
+		m_FileStream.write(reinterpret_cast<char*>(bytes), 8);
+	}
+
+	void FileWriter::WriteString(const std::string& str)
+	{
+		m_FileStream.write(str.c_str(), str.size());
+	}
+
+	void FileWriter::WriteStringWithNull(const std::string& str)
+	{
+		m_FileStream.write(str.c_str(), str.size() + 1);
+	}
+}
---
+++ PyroEngine/src/PyroEngine/File/FileWriter.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <stdint.h>
+
+//The limit (in bytes) of which a file can be opened using a FileWriter
+#define PYRO_FILE_WRITER_FILE_LIMIT 104857600
+
+namespace PyroEngine
+{
+	//Object that can be used to write to files on the host system
+	class FileWriter
+	{
+	private:
+		std::filesystem::path m_FilePath; //The path to where the currently open file is on the host system
+		std::ofstream m_FileStream; //The output stream used to open, close and write to files
+		bool m_IsAlwaysAppending; //True if every write operation will append to the end of the file
+	public:
+		//Default constructor
+		FileWriter();
+		//Default destructor
+		~FileWriter();
+
+		//Returns the absolute path of the currently open file. If no file is open, then it will return the current working directory
+		std::filesystem::path GetFilePath() const;
+		//Returns whether or not every write operation will append to the end of the file. If no file is open, then it will return false always
+		bool GetIsAlwaysAppending() const;
+
+		//Opens a file on the host system. If a file is already currently open then it will close that file before attempting to open the new one. The writing indicator will be set at the very beginning of the file so that the next written byte is at the very start of the file. An optional flag can be given to specify that every write operation to the new file will append to the end of the file no matter what. This function can not open files that are greater in size than the PYRO_FILE_WRITER_FILE_LIMIT macro except if you are opening the file to append always. Returns whether or not the file has been successfully opened
+		bool Open(const std::filesystem::path& path, bool alwaysAppend = false);
+		//Closes the currently open file. If no file is open, then this function will not do anything
+		void Close();
+		//Returns whether or not a file is currently open
+		bool IsOpen() const;
+
+		//Sets the position of the indicator such that the next written byte is from that position. Has no effect if the currently open file is being appended to always
+		void SeekPosition(size_t pos);
+		//Retrieves the current position of the indicator within the currently open file. If no file is open then it will return ???
+		size_t GetPosition();
+
+		//Sets the position of the indicator such that the next written byte is at the very start of the currently open file
+		void GoToStart();
+		//Sets the position of the indicator such that the next written byte is at the very end of the currently open file (any write operations after this will write new bytes to the file, not change the ones that are already there)
+		void GoToEnd();
+
+		//Writes an 8-bit signed integer (little-endian) to the current position within the file and increments said position by 1 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 1
+		void WriteInt8LE(int8_t integer);
+		//Writes a 16-bit signed integer (little-endian) to the current position within the file and increments said position by 2 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 2
+		void WriteInt16LE(int16_t integer);
+		//Writes a 32-bit signed integer (little-endian) to the current position within the file and increments said position by 4 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 4
+		void WriteInt32LE(int32_t integer);
+		//Writes a 64-bit signed integer (little-endian) to the current position within the file and increments said position by 8 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 8
+		void WriteInt64LE(int64_t integer);
+
+		//Writes an 8-bit unsigned integer (little-endian) to the current position within the file and increments said position by 1 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 1
+		void WriteUInt8LE(uint8_t integer);
+		//Writes a 16-bit unsigned integer (little-endian) to the current position within the file and increments said position by 2 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 2
+		void WriteUInt16LE(uint16_t integer);
+		//Writes a 32-bit unsigned integer (little-endian) to the current position within the file and increments said position by 4 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 4
+		void WriteUInt32LE(uint32_t integer);
+		//Writes a 64-bit unsigned integer (little-endian) to the current position within the file and increments said position by 8 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 8
+		void WriteUInt64LE(uint64_t integer);
+
+		//Writes an 8-bit signed integer (big-endian) to the current position within the file and increments said position by 1 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 1
+		void WriteInt8BE(int8_t integer);
+		//Writes a 16-bit signed integer (big-endian) to the current position within the file and increments said position by 2 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 2
+		void WriteInt16BE(int16_t integer);
+		//Writes a 32-bit signed integer (big-endian) to the current position within the file and increments said position by 4 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 4
+		void WriteInt32BE(int32_t integer);
+		//Writes a 64-bit signed integer (big-endian) to the current position within the file and increments said position by 8 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 8
+		void WriteInt64BE(int64_t integer);
+
+		//Writes an 8-bit unsigned integer (big-endian) to the current position within the file and increments said position by 1 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 1
+		void WriteUInt8BE(uint8_t integer);
+		//Writes a 16-bit unsigned integer (big-endian) to the current position within the file and increments said position by 2 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 2
+		void WriteUInt16BE(uint16_t integer);
+		//Writes a 32-bit unsigned integer (big-endian) to the current position within the file and increments said position by 4 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 4
+		void WriteUInt32BE(uint32_t integer);
+		//Writes a 64-bit unsigned integer (big-endian) to the current position within the file and increments said position by 8 if it can. If it can't then it will increment by as much as it possibly can up to a maximum of 8
+		void WriteUInt64BE(uint64_t integer);
+
+		//Writes a string to the file ignoring the null character at the end, then increments the positional indicator by the string's length
+		void WriteString(const std::string& str);
+		//Writes a string to the file including the null character at the end, then increments the positional indicator by the string's length
+		void WriteStringWithNull(const std::string& str);
+	};
+}
